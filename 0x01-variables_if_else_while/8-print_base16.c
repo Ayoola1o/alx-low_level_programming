0ecdc1fa0744da858b4d73f89b9cc2aad34d386e
root <Ayoola1o>
2022-10-14 19:52:33 -0700

    testing

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,12 @@
 int main(void)
 {
 	int d;
-	char low;
 
+	char low;
 	for (d = '0'; d <= '9'; d++)
-		putchar(d);
+	putchar(d);
 	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
 		putchar('\n');
-
 	return (0);
 }
