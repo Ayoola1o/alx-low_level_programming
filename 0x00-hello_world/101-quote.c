52142aaee76343048ab3f3c8401aae91d62f1baf
root <Ayoola1o>
2022-10-16 15:59:26 -0700

    task12

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);		 return (1);
-}
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
+	return (1);
+}
