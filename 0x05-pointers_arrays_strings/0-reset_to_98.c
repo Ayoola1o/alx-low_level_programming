973d7e3a771ccb1bb82443b424a69a0da5c7c6e0
root <Ayoola1o>
2022-10-24 16:22:22 -0700

    update pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Set value of a variable to 98 using a pointer
+ * @n: The address of the value needed to change
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
